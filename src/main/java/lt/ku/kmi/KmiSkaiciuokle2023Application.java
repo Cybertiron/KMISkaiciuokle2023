470d77da06c1badfc61edf1c2526bb05d75d62af
ltwarrior18 <ltwarrior18@gmail.com>
2023-05-10 10:39:26 +0300

    KMI 100%

---
+++ src/main/java/lt/ku/kmi/KmiSkaiciuokle2023Application.java
@@ -0,0 +1,13 @@
+package lt.ku.kmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmiSkaiciuokle2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KmiSkaiciuokle2023Application.class, args);
+    }
+
+}
